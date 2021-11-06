57fdde0256630236fc3556bfab19b620b9af79b4
Josh Noel <obsidize@gmail.com>
2021-11-06 14:56:03 -0500

    add utility methods to FastNoisePipelineModule for autofilling from Vector*** constructs

--- Runtime/FastNoisePipelineModule.cs
+++ Runtime/FastNoisePipelineModule.cs
@@ -4,8 +4,29 @@ namespace Obsidize.FastNoise
 {
 	public abstract class FastNoisePipelineModule : ScriptableObject
 	{
+
 		public abstract void SetSeed(int seed);
 		public abstract float GetNoise(float x, float y);
 		public abstract float GetNoise(float x, float y, float z);
+
+		public float GetNoise(Vector2 position)
+		{
+			return GetNoise(position.x, position.y);
+		}
+
+		public float GetNoise(Vector2Int position)
+		{
+			return GetNoise(position.x, position.y);
+		}
+
+		public float GetNoise(Vector3 position)
+		{
+			return GetNoise(position.x, position.y, position.z);
+		}
+
+		public float GetNoise(Vector3Int position)
+		{
+			return GetNoise(position.x, position.y, position.z);
+		}
 	}
 }
